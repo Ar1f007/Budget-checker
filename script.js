70d80cd9cc0ea594e2ef4607aa0920ff8e12756d
Ar1f007 <muhammadarif9416@yahoo.com>
2022-02-17 18:28:12 +0600

    get the input on form submit and check if the value are valid

--- script.js
+++ script.js
@@ -6,3 +6,37 @@ const clothesInput = document.querySelector('#clothes');
 const calculateBtn = document.querySelector('#calculate');
 const totalExpenseEl = document.querySelector('#total-expense');
 const balanceEl = document.querySelector('#balance');
+
+let incomeAmount = null,
+  foodCost = null,
+  rentCost = null,
+  clothesCost = null;
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  getTheInput();
+
+  isValid(incomeAmount, foodCost, rentCost, clothesCost);
+}
+
+function getTheInput() {
+  incomeAmount = incomeInput.value;
+  foodCost = foodInput.value;
+  rentCost = rentInput.value;
+  clothesCost = clothesInput.value;
+}
+
+function isValid(income, food, rent, clothes) {
+  if (isNaN(income) || isNaN(food) || isNaN(rent) || isNaN(clothes)) {
+    showError(form, 'Please enter a number input value');
+    return;
+  }
+
+  if (income < 0 || food < 0 || rent < 0 || clothes < 0) {
+    showError(form, 'Please enter a number input greater than 0(zero)');
+    return;
+  }
+}
+
+calculateBtn.addEventListener('click', handleSubmit);
