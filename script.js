2c23318ff82290222a28acda6629b73022127cd1
Ar1f007 <muhammadarif9416@yahoo.com>
2022-02-17 22:32:18 +0600

    added comments to each blocks and functions and clearTimeout function for setting new time to the error

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Getting all the elements related to income-expenses
 const form = document.querySelector('form');
 const incomeInput = document.querySelector('#income');
 const foodInput = document.querySelector('#food');
@@ -7,6 +8,7 @@ const totalExpensePlaceholder =
   document.querySelector('#total-expense').firstElementChild;
 const balancePlaceholder = document.querySelector('#balance').firstElementChild;
 
+// SPECIFIC TO SAVINGS
 const savingsInput = document.querySelector('#save');
 const budgetInfoEl = document.querySelector('#budget-info');
 
@@ -15,39 +17,50 @@ const savingsAmountPlaceholder =
 const remainingBalancePlaceholder =
   document.querySelector('#remaining-balance').firstElementChild;
 
+// BUTTONS
 const calculateBtn = document.querySelector('#calculate');
 const saveBtn = document.querySelector('#save-btn');
 const clearBtn = document.querySelector('#clear');
 
+// INITIAL VALUE
 let incomeAmount = null,
   foodCost = null,
   rentCost = null,
   clothesCost = null,
-  balance = null;
+  balance = null,
+  timer = null;
 
+// ON FORM SUBMIT / CLICKED ON CALCULATE
 function handleSubmit(e) {
   e.preventDefault();
 
   totalExpensePlaceholder.textContent = '';
   balancePlaceholder.textContent = '';
+
+  // REMOVE ANY PREVIOUS ERRORS FROM THE DOM ON FORM SUBMIT
   removePreviousError();
 
   getTheInput();
 
+  // CHECKING IF THE VALUES ARE VALID - then proceeds
   if (isValid(incomeAmount, foodCost, rentCost, clothesCost)) {
     calculateTotalExpenseAndBalance();
   }
 }
 
+// REMOVES ANY PREVIOUS ERRORS
 function removePreviousError() {
   const errors = Array.from(document.querySelectorAll('.error'));
 
   if (errors) {
     for (const error of errors) {
+      clearTimeout(timer);
       error.remove();
     }
   }
 }
+
+// GET THE INPUT FROM EXPENSES AND INCOME FIELDS
 function getTheInput() {
   incomeAmount = incomeInput.value;
   foodCost = foodInput.value;
@@ -55,19 +68,31 @@ function getTheInput() {
   clothesCost = clothesInput.value;
 }
 
-function clearInput(e) {
-  e.preventDefault();
-  incomeInput.value = '';
-  foodInput.value = '';
-  rentInput.value = '';
-  clothesInput.value = '';
-  savingsInput.value = '';
-  totalExpensePlaceholder.textContent = '';
-  balancePlaceholder.textContent = '';
-  savingsAmountPlaceholder.textContent = '';
-  remainingBalancePlaceholder.textContent = '';
+// CHECKS THE VALIDITY OF INPUTS - shows output accordingly
+// return true indicating inputs are valid, and false indicates not valid
+/* 1. isNaN() for checking if the values are number or not
+   2. input < 0 for checking if given input is negative
+   3. input == '' for, incase no input is provided
+*/
+function isValid() {
+  const inputs = arguments;
+  for (const input of inputs) {
+    if (isNaN(input) || input < 0 || input === '') {
+      if (isNaN(input)) {
+        showError(form, 'Please enter a number input value');
+      } else if (input < 0) {
+        showError(form, 'Please enter a number input greater than 0(zero)');
+      } else {
+        showError(form, 'Please provide inputs');
+      }
+      return false;
+    }
+  }
+
+  return true;
 }
 
+// CALCULATES TOTAL EXPENSE AND BALANCE AND SHOWS OUTPUT TO THE PAGE
 function calculateTotalExpenseAndBalance() {
   const totalExpenses =
     Number(foodCost) + Number(rentCost) + Number(clothesCost);
@@ -87,24 +112,7 @@ function calculateTotalExpenseAndBalance() {
   }
 }
 
-function isValid() {
-  const inputs = arguments;
-  for (const input of inputs) {
-    if (isNaN(input) || input < 0 || input === '') {
-      if (isNaN(input)) {
-        showError(form, 'Please enter a number input value');
-      } else if (input < 0) {
-        showError(form, 'Please enter a number input greater than 0(zero)');
-      } else {
-        showError(form, 'Please provide inputs');
-      }
-      return false;
-    }
-  }
-
-  return true;
-}
-
+// HANDLES SHOWING ERROR
 function showError(parent, msg) {
   const p = document.createElement('p');
   p.classList.add('error');
@@ -112,20 +120,21 @@ function showError(parent, msg) {
 
   parent.appendChild(p);
 
-  setTimeout(() => {
+  // REMOVE ERRORS AFTER 4 SECONDS
+  timer = setTimeout(() => {
     removeError(parent);
-  }, 5000);
+  }, 4000);
 }
 
+// REMOVES THE ERROR FROM PAGE IF THERE IS ANY
 function removeError(parent) {
-  // const errorMsg = parent.querySelector('.error');
-  // parent.removeChild(errorMsg);
   const errorEl = parent.querySelector('.error');
   if (errorEl) {
     errorEl.remove();
   }
 }
 
+// CALCULATE SAVINGS - SAVING AMOUNT AND REMAINING BALANCE - SHOWS VALUE TO THE PAGE
 function calculateSavings() {
   removePreviousError();
   savingsAmountPlaceholder.textContent = '';
@@ -134,7 +143,6 @@ function calculateSavings() {
   const savingsPercentage = savingsInput.value;
   if (isValid(savingsPercentage)) {
     const savingAmount = (savingsPercentage * incomeAmount) / 100;
-    console.log(savingAmount);
 
     if (savingAmount > balance || savingAmount == 0) {
       showError(budgetInfoEl, 'Insufficient amount');
@@ -143,12 +151,26 @@ function calculateSavings() {
 
     if (savingAmount) {
       savingsAmountPlaceholder.textContent = savingAmount;
-
       remainingBalancePlaceholder.textContent = balance - savingAmount;
     }
   }
 }
 
+// CLEARING ALL THE VALUES/PLACEHOLDERS
+function clearInput(e) {
+  e.preventDefault();
+  incomeInput.value = '';
+  foodInput.value = '';
+  rentInput.value = '';
+  clothesInput.value = '';
+  savingsInput.value = '';
+  totalExpensePlaceholder.textContent = '';
+  balancePlaceholder.textContent = '';
+  savingsAmountPlaceholder.textContent = '';
+  remainingBalancePlaceholder.textContent = '';
+}
+
+// EVENT LISTENERS
 calculateBtn.addEventListener('click', handleSubmit);
 saveBtn.addEventListener('click', calculateSavings);
 clearBtn.addEventListener('click', clearInput);
